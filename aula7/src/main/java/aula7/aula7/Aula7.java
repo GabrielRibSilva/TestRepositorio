dc95698b804b38c1ded6933f8990032088699edc
GabrielRibSilva <gcrs2018@gmail.com>
2024-03-01 21:33:26 -0300

    Criei o arquivo Index em 01/03/2024

---
+++ aula7/src/main/java/aula7/aula7/Aula7.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package aula7.aula7;
+
+/**
+ *
+ * @author Gabriel Cândido <gcrs2018@gmail.com>
+ */
+public class Aula7 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ aula7/src/main/java/exercicioum/TestGitHub.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package exercicioum;
+
+/**
+ *
+ * @author Gabriel Cândido <gcrs2018@gmail.com>
+ * @date 01/03/2024
+ * @brief Class TestGitHub
+ */
+public class TestGitHub {
+
+}
